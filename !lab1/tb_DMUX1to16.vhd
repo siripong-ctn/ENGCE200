ceae0d5f072c0dc233a090cb7d0757e6e0592334
siripong-ctn <siripong.2320@gmail.com>
2026-07-16 23:53:35 +0700

    Success lab1 first try

--- !lab1/tb_DMUX1to16.vhd
+++ !lab1/tb_DMUX1to16.vhd
@@ -6,8 +6,12 @@ end tb_DMUX1to16;
 
 architecture sim of tb_DMUX1to16 is
 
-    signal D : STD_LOGIC;
+    signal D : STD_LOGIC := '0';
     signal S : STD_LOGIC_VECTOR(3 downto 0);
+
+    signal Y : STD_LOGIC_VECTOR(15 downto 0);
+
+    -- แยกออกมาเพื่อดูทีละเส้นใน GTKWave
     signal Y0  : STD_LOGIC;
     signal Y1  : STD_LOGIC;
     signal Y2  : STD_LOGIC;
@@ -34,58 +38,48 @@ begin
         Y => Y
     );
 
+    -- แตก Bus ออกเป็นรายบิต
+    Y0  <= Y(0);
+    Y1  <= Y(1);
+    Y2  <= Y(2);
+    Y3  <= Y(3);
+    Y4  <= Y(4);
+    Y5  <= Y(5);
+    Y6  <= Y(6);
+    Y7  <= Y(7);
+    Y8  <= Y(8);
+    Y9  <= Y(9);
+    Y10 <= Y(10);
+    Y11 <= Y(11);
+    Y12 <= Y(12);
+    Y13 <= Y(13);
+    Y14 <= Y(14);
+    Y15 <= Y(15);
+
     process
     begin
 
         D <= '1';
 
-        S <= "0000";
-        wait for 10 ns;
+        S <= "0000"; wait for 10 ns;
+        S <= "0001"; wait for 10 ns;
+        S <= "0010"; wait for 10 ns;
+        S <= "0011"; wait for 10 ns;
 
-        S <= "0001";
-        wait for 10 ns;
+        S <= "0100"; wait for 10 ns;
+        S <= "0101"; wait for 10 ns;
+        S <= "0110"; wait for 10 ns;
+        S <= "0111"; wait for 10 ns;
 
-        S <= "0010";
-        wait for 10 ns;
+        S <= "1000"; wait for 10 ns;
+        S <= "1001"; wait for 10 ns;
+        S <= "1010"; wait for 10 ns;
+        S <= "1011"; wait for 10 ns;
 
-        S <= "0011";
-        wait for 10 ns;
-
-        S <= "0100";
-        wait for 10 ns;
-
-        S <= "0101";
-        wait for 10 ns;
-
-        S <= "0110";
-        wait for 10 ns;
-
-        S <= "0111";
-        wait for 10 ns;
-
-        S <= "1000";
-        wait for 10 ns;
-
-        S <= "1001";
-        wait for 10 ns;
-
-        S <= "1010";
-        wait for 10 ns;
-
-        S <= "1011";
-        wait for 10 ns;
-
-        S <= "1100";
-        wait for 10 ns;
-
-        S <= "1101";
-        wait for 10 ns;
-
-        S <= "1110";
-        wait for 10 ns;
-
-        S <= "1111";
-        wait for 10 ns;
+        S <= "1100"; wait for 10 ns;
+        S <= "1101"; wait for 10 ns;
+        S <= "1110"; wait for 10 ns;
+        S <= "1111"; wait for 10 ns;
 
         D <= '0';
 
