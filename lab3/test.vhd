79f9e8843436635edf1aa324263f98ff576c4761
siripong-ctn <siripong.2320@gmail.com>
2026-09-04 01:07:05 +0700

    Update code lab3

--- lab3/test.vhd
+++ lab3/test.vhd
@@ -21,17 +21,13 @@ begin
         SEL := A2 & A1;
 
         case SEL is
-            when "00" =>
-                A_more_B <= (A0 and not B2 and not B1 and not B0);
+            when "00" => A_more_B <= (A0 and not B2 and not B1 and not B0);
 
-            when "01" =>
-                A_more_B <= ((not B2 and not B1) or (A0 and not B2 and B1 and not B0));
+            when "01" => A_more_B <= ((not B2 and not B1) or (A0 and not B2 and B1 and not B0));
 
-            when "10" =>
-                A_more_B <= (B2 or (A0 and B2 and not B1 and not B0));
+            when "10" => A_more_B <= (B2 or (A0 and B2 and not B1 and not B0));
 
-            when others =>
-                A_more_B <= (not B2 or not B1 or (A0 and B2 and B1 and not B0));
+            when others => A_more_B <= (not B2 or not B1 or (A0 and B2 and B1 and not B0));
         end case;
     end process;
 
@@ -42,24 +38,13 @@ begin
         SEL := A2 & A1;
 
         case SEL is
-            when "00" =>
-                A_less_B <=
-                    (B1 or B2 or
-                     (not A0 and not B2 and not B1 and B0));
+            when "00" => A_less_B <= (B1 or B2 or (not A0 and not B2 and not B1 and B0));
 
-            when "01" =>
-                A_less_B <=
-                    (B2 or
-                     (not A0 and not B2 and B1 and B0));
+            when "01" => A_less_B <= (B2 or (not A0 and not B2 and B1 and B0));
 
-            when "10" =>
-                A_less_B <=
-                    ((B2 and B1) or
-                     (not A0 and B2 and not B1 and B0));
+            when "10" => A_less_B <= ((B2 and B1) or (not A0 and B2 and not B1 and B0));
 
-            when others =>
-                A_less_B <=
-                    (not A0 and B2 and B1 and B0);
+            when others => A_less_B <= (not A0 and B2 and B1 and B0);
         end case;
     end process;
 
@@ -70,25 +55,13 @@ begin
         SEL := A2 & A1;
 
         case SEL is
-            when "00" =>
-                A_equal_B <=
-                    ((not A0 and not B2 and not B1 and not B0) or
-                     (A0 and not B2 and not B1 and B0));
+            when "00" => A_equal_B <= ((not A0 and not B2 and not B1 and not B0) or (A0 and not B2 and not B1 and B0));
 
-            when "01" =>
-                A_equal_B <=
-                    ((A0 and not B2 and B1 and B0) or
-                     (not A0 and not B2 and B1 and not B0));
+            when "01" => A_equal_B <= ((A0 and not B2 and B1 and B0) or (not A0 and not B2 and B1 and not B0));
 
-            when "10" =>
-                A_equal_B <=
-                    ((not A0 and B2 and not B1 and not B0) or
-                     (A0 and B2 and not B1 and B0));
+            when "10" => A_equal_B <= ((not A0 and B2 and not B1 and not B0) or (A0 and B2 and not B1 and B0));
 
-            when others =>
-                A_equal_B <=
-                    ((A0 and B2 and B1 and B0) or
-                     (not A0 and B2 and B1 and not B0));
+            when others => A_equal_B <= ((A0 and B2 and B1 and B0) or (not A0 and B2 and B1 and not B0));
         end case;
     end process;
 
